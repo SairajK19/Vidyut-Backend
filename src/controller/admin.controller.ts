d8e81e612b43253b19abdbd5e0da825c1a13cfbc
Sheetal Kapri <skapri@oneshield.com>
2023-08-02 15:30:57 +0530

    fetching consumers

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -1,7 +1,21 @@
 import { Router } from "express";
+import {ConsumerFetchDetails, fetchConsumer} from "../services/admin.service";
+import { User } from "../models";
 
 export const adminRouter = Router();
 
-adminRouter.get("/", (req, res) => {
-    res.send("Admin Working")
+adminRouter.get("/fetchConsumers", async (req, res) => {
+    try {
+        const fetchedConsumers: Array<ConsumerFetchDetails> = await fetchConsumer();
+        fetchedConsumers ? res.status(200).json({ success: true, message: "fetched consumer successfully", fetchedConsumerIds: fetchedConsumers })
+            : res.status(500).json({ success: false, message: "consumer fetching failed" })
+
+    } catch (err){
+        console.log(err)
+        res.status(500).json({success:false,message:"internal server error"})
+    }
+
 })
+
+
+
