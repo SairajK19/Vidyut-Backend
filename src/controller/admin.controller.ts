56e303e9fb4a8e76da1f662daf73f849a020d501
SairajK19 <saikapdi11@gmail.com>
2023-08-02 15:18:03 +0530

    added router to get application details

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -1,7 +1,22 @@
 import { Router } from "express";
+import { consumerCollection } from "../services/initDb";
 
 export const adminRouter = Router();
 
-adminRouter.get("/", (req, res) => {
-    res.send("Admin Working")
+adminRouter.get("/consumerApplicationDetails/:consumerId", async (req, res) => {
+    try {
+        const consumerId: string = req.params.consumerId;
+        const consumerApplication = (await consumerCollection.doc(consumerId).get()).data()
+
+        consumerApplication
+            ? res.status(200).json({ success: true, message: "Consumer application found", consumerApplication: consumerApplication })
+            : res.status(500).json({ success: false, message: `Failed to get application details of consumer ${consumerId}` })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: "Failed to get consumer application details",
+            error: err,
+            success: false
+        })
+    }
 })
